3375993b7bd6b9fbe5d16cd864955c6207920b80
Christos Trochalakis <yatiohi@ideopolis.gr>
2024-03-12 21:52:29 +0200

    provide a default package

--- flake.nix
+++ flake.nix
@@ -16,11 +16,13 @@
     } // utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.standalone-firefox = pkgs.callPackage ./firefox.nix {
+        firefoxPackage = pkgs.callPackage ./firefox.nix {
           inherit pkgs;
           arch = "linux-x86_64";
         };
+      in
+      {
+        packages.standalone-firefox = firefoxPackage;
+        packages.default = firefoxPackage;
       });
 }
